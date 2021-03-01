941c7fb516482d636e8df7eb747b27e38b4fc2a0
nitin.gupta <nitin.gupta@compass.com>
2021-03-01 22:36:24 +0530

    Fixed out of memory issue

--- train_deploy.py
+++ train_deploy.py
@@ -308,10 +308,10 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--batch-size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
+        "--batch_size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
     )
     parser.add_argument(
-        "--test-batch-size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
+        "--test_batch_size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
     )
     parser.add_argument("--epochs", type=int, default=2, metavar="N", help="number of epochs to train (default: 10)")
     parser.add_argument("--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)")
